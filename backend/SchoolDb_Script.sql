e84132366e0eb5eed11bbac19d1369fe53341319
Laura Chica <laurachica282@gmail.com>
2026-09-03 12:33:52 -0500

    refactor: mover el backend a la carpeta backend/

---
+++ backend/SchoolDb_Script.sql
@@ -0,0 +1,45 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Students] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(100) NOT NULL,
+    CONSTRAINT [PK_Students] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [Teachers] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(100) NOT NULL,
+    CONSTRAINT [PK_Teachers] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [Grades] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(100) NOT NULL,
+    [Value] decimal(4,2) NOT NULL,
+    [StudentId] int NOT NULL,
+    [TeacherId] int NOT NULL,
+    CONSTRAINT [PK_Grades] PRIMARY KEY ([Id]),
+    CONSTRAINT [CK_Grades_Value] CHECK ([Value] >= 0 AND [Value] <= 5),
+    CONSTRAINT [FK_Grades_Students] FOREIGN KEY ([StudentId]) REFERENCES [Students] ([Id]) ON DELETE NO ACTION,
+    CONSTRAINT [FK_Grades_Teachers] FOREIGN KEY ([TeacherId]) REFERENCES [Teachers] ([Id]) ON DELETE NO ACTION
+);
+
+CREATE INDEX [IX_Grades_StudentId] ON [Grades] ([StudentId]);
+
+CREATE INDEX [IX_Grades_TeacherId] ON [Grades] ([TeacherId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260903164134_InitialCreate', N'10.0.11');
+
+COMMIT;
+GO
+
